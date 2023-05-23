8dbe0272a3312f6529f9e76b8609ec41fb261800
Jun Park <pj2417@gmail.com>
2023-05-23 21:39:57 +0900

    add supported chain

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -1,5 +1,24 @@
 import { MUDChain, latticeTestnet } from "@latticexyz/common/chains";
 import { foundry } from "@wagmi/chains";
 
+export const hackathonTestnet = {
+    name: "Hackathon Testnet",
+    id: 16464,
+    network: "hackathon-testnet",
+    nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
+    rpcUrls: {
+      default: {
+        http: ["https://lattice-goerli-sequencer.optimism.io"],
+        webSocket: ["wss://lattice-goerli-sequencer.optimism.io"],
+      },
+      public: {
+        http: ["https://lattice-goerli-sequencer.optimism.io"],
+        webSocket: ["wss://lattice-goerli-sequencer.optimism.io"],
+      },
+    },
+    // modeUrl: "https://mode.testnet-mud-services.linfra.xyz",
+    faucetUrl: "https://faucet.hackathon-mud-services.linfra.xyz",
+  } as const satisfies MUDChain;
+
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [foundry, latticeTestnet];
+export const supportedChains: MUDChain[] = [foundry, latticeTestnet, hackathonTestnet];
